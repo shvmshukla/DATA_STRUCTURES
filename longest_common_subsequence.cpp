56521c5712875710a295da714db645235d82dac7
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2017-11-04 10:19:43 +0530

    Update longest_common_subsequence.cpp

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -1,5 +1,8 @@
+//longest common subsequence with backtracking as well
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int max(int a,int b);
 int max(int a,int b)
 {
@@ -7,18 +10,16 @@ int max(int a,int b)
 }
 int main()
 {
-	int n,m;
+	int n,m,i,j;
 	scanf("%d%d",&n,&m);
 	char t1[n],t2[m];
 	scanf("%s", t1);
 	scanf("%s", t2);
-//	n=strlen(t1);
-//	m=strlen(t2);
 	int table[n+1][m+1];
 	
-	for(int i=0;i<n+1;i++)
+	for(i=0;i<n+1;i++)
 	{
-		for(int j=0;j<m+1;j++)
+		for(j=0;j<m+1;j++)
 		{
 			if(i==0 || j==0)
 				table[i][j]=0;
@@ -28,7 +29,41 @@ int main()
 				table[i][j]=max(table[i][j-1],table[i-1][j]);
 		}
 	}
-	printf("%d\n",table[n][m]);
-
+	for(i=0;i<n+1;i++)
+	{
+		for(j=0;j<m+1;j++)
+		{
+			printf("%d\t",table[i][j]);
+		}
+		printf("\n");
+	}
+	printf("%d\n",table[n][m] );
+	i=n;
+	j=m;
+	int p=0;
+	char res[n>m?m+1:n+1];
+	while(i>0&& j>0)
+	{
+		if(t1[i-1]==t2[j-1])
+		{
+			res[p++]=t1[i-1];
+			i--;
+			j--;
+		}
+		else
+		{
+			if(table[i-1][j]>table[i][j-1])
+				i--;
+			else 
+				j--;
+		}
+	}
+	res[p]='\0';
+	printf("Longest Common Subsequence = ");
+	while(p)
+	{
+		printf("%c",res[--p] );
+	}
+	printf("\n");
 	return 0;
 }
